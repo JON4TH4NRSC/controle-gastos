01ba95ac2f1c1a1e84a2bb890a5cd5ff257f7c79
Jonathan <jonathanrodcorrea@gmail.com>
2026-05-17 23:01:31 -0300

    Melhorando interface do controle de gastos

--- public/script.js
+++ public/script.js
@@ -1,44 +1,114 @@
 async function carregar() {
+
   const res = await fetch('/gastos');
   const gastos = await res.json();
 
   const lista = document.getElementById('lista');
+
   lista.innerHTML = '';
 
   gastos.forEach((g, i) => {
+
     const li = document.createElement('li');
-    li.innerHTML = `${g.nome} - R$ ${g.valor} <button onclick="remover(${i})">X</button>`;
+
+    li.innerHTML = `
+      <span>
+        💸 ${g.nome} - 
+        R$ ${Number(g.valor).toFixed(2)}
+      </span>
+
+      <button onclick="remover(${i})">
+        X
+      </button>
+    `;
+
     lista.appendChild(li);
+
   });
 
   const totalRes = await fetch('/total');
+
   const totalData = await totalRes.json();
-  document.getElementById('total').innerText = totalData.total;
+
+  document.getElementById('total').innerText =
+    `R$ ${Number(totalData.total).toFixed(2)}`;
+
 }
 
+/* ADICIONAR */
 async function adicionar() {
 
-  const nome = document.getElementById('nome').value;
+  const nome =
+    document.getElementById('nome').value.trim();
 
   const valor = parseFloat(
     document.getElementById('valor').value
   );
 
+  if(!nome || isNaN(valor)){
+
+    alert('Preencha os campos');
+
+    return;
+  }
+
   await fetch('/gastos', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ nome, valor })
+
+    method:'POST',
+
+    headers:{
+      'Content-Type':'application/json'
+    },
+
+    body: JSON.stringify({
+      nome,
+      valor
+    })
+
   });
 
   document.getElementById('nome').value = '';
   document.getElementById('valor').value = '';
 
   carregar();
+
 }
 
-async function remover(id) {
-  await fetch(`/gastos/${id}`, { method: 'DELETE' });
+/* REMOVER */
+async function remover(id){
+
+  await fetch(`/gastos/${id}`, {
+    method:'DELETE'
+  });
+
   carregar();
+
+}
+
+/* COTAÇÕES */
+async function carregarCotacoes() {
+
+  try {
+
+    const resposta = await fetch(
+      'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL'
+    );
+
+    const dados = await resposta.json();
+
+    document.getElementById('dolar').innerHTML =
+      `💵 Dólar<br>R$ ${Number(dados.USDBRL.bid).toFixed(2)}`;
+
+    document.getElementById('euro').innerHTML =
+      `💶 Euro<br>R$ ${Number(dados.EURBRL.bid).toFixed(2)}`;
+
+  } catch (erro) {
+
+    console.log(erro);
+
+  }
+
 }
 
 carregar();
+carregarCotacoes();
