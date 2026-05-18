18ab38677a98634217eebd367a93378ead31200e
Jonathan <jonathanrodcorrea@gmail.com>
2026-05-17 23:56:39 -0300

    Corrigindo rota principal

--- src/server.js
+++ src/server.js
@@ -1,29 +1,70 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-const { adicionarGasto, listarGastos, removerGasto, calcularTotal } = require('./main');
+const {
+  adicionarGasto,
+  listarGastos,
+  removerGasto,
+  calcularTotal
+} = require('./main');
 
 app.use(express.json());
+
+/* ARQUIVOS ESTÁTICOS */
 app.use(express.static('public'));
 
+/* ROTA PRINCIPAL */
+app.get('/', (req, res) => {
+
+  res.sendFile(
+    path.join(__dirname, '../public/index.html')
+  );
+
+});
+
+/* LISTAR GASTOS */
 app.get('/gastos', (req, res) => {
+
   res.json(listarGastos());
+
 });
 
+/* ADICIONAR */
 app.post('/gastos', (req, res) => {
+
   const { nome, valor } = req.body;
+
   adicionarGasto(nome, Number(valor));
+
   res.sendStatus(200);
+
 });
 
+/* REMOVER */
 app.delete('/gastos/:id', (req, res) => {
+
   removerGasto(Number(req.params.id));
+
   res.sendStatus(200);
+
 });
 
+/* TOTAL */
 app.get('/total', (req, res) => {
-  res.json({ total: calcularTotal() });
+
+  res.json({
+    total: calcularTotal()
+  });
+
 });
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+/* SERVIDOR */
+app.listen(3000, () => {
+
+  console.log(
+    'Servidor rodando em http://localhost:3000'
+  );
+
+});
